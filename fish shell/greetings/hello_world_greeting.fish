63eb16cec64319b7c6c351ccb8105f0b0e444224
fernanda <fmserrat@gmail.com>
2020-04-22 19:30:51 -0400

    Some terminal greetings.

---
+++ fish shell/greetings/hello_world_greeting.fish
@@ -0,0 +1,18 @@
+function fish_greeting -d "Greeting message on shell session start up"
+    set_color blue
+    echo ""
+    echo -en "  ,-.       _,---._ __  / \\n"
+    echo -en " /  )    .-'       `./ /   \\n"
+    echo -en "(  (   ,'            `/    /|\n"
+    echo -en " \  `-"             \'\   / |\n"
+    echo -en "  `.              ,  \ \ /  |\n"
+    echo -en "   /`.          ,'-`----Y   |\n"
+    echo -en "  (            ;        |   '\n"
+    echo -en "  |  ,-.    ,-'         |  /\n"
+    echo -en "  |  | (   |       fmst | /\n"
+    echo -en "  )  |  \  `.___________|/\n"
+    echo -en "  `--'   `--'\n"
+    echo -en "\n"
+    
+end
+
