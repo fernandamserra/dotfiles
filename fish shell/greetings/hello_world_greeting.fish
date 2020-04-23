18cdce0e3558d326fe7162b619adf4a1da83ac3b
fernanda <fmserrat@gmail.com>
2020-04-22 20:25:39 -0400

    update hello world greeting with kawaii emojii.

--- fish shell/greetings/hello_world_greeting.fish
+++ fish shell/greetings/hello_world_greeting.fish
@@ -1,18 +1,14 @@
 function fish_greeting -d "Greeting message on shell session start up"
     set_color blue
     echo ""
-    echo -en "  ,-.       _,---._ __  / \\n"
-    echo -en " /  )    .-'       `./ /   \\n"
-    echo -en "(  (   ,'            `/    /|\n"
-    echo -en " \  `-"             \'\   / |\n"
-    echo -en "  `.              ,  \ \ /  |\n"
-    echo -en "   /`.          ,'-`----Y   |\n"
-    echo -en "  (            ;        |   '\n"
-    echo -en "  |  ,-.    ,-'         |  /\n"
-    echo -en "  |  | (   |       fmst | /\n"
-    echo -en "  )  |  \  `.___________|/\n"
-    echo -en "  `--'   `--'\n"
-    echo -en "\n"
+    echo -en "           _   _      _ _        __        __         _     _ _ _\n"
+    echo -en "          | | | | ___| | | ___   \ \      / /__  _ __| | __| | | |\n"
+    echo -en "          | |_| |/ _ \ | |/ _ \   \ \ /\ / / _ \| '__| |/ _` | | |\n"
+    echo -en "          |  _  |  __/ | | (_) |   \ V  V / (_) | |  | | (_| |_|_|\n"
+    echo -en "          |_| |_|\___|_|_|\___/     \_/\_/ \___/|_|  |_|\__,_(_|_)\n\n"
+    set_color magenta
+    echo -en "(づ｡◕‿‿◕｡)づ\n"
+    echo ""
     
 end
 
